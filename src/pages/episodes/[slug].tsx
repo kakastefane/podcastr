62c1f74bcf69038d45a59ef4e75065bba7cb70a8
kakastefane <karistonstefane@hotmail.com>
2021-04-22 15:11:07 -0300

    Finalizei a pagina do episodio com a geracao estatica dinamica, dei inicio a criacao do player com as funcoes de play e pause, trabalhei com contextAPI e refs

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,7 +3,6 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router'
 
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
@@ -27,8 +26,6 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
-  const router = useRouter();
-
   return (
     <div className={styles.wrapperEpisode}>
       <div className={styles.episode}>
@@ -65,8 +62,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
